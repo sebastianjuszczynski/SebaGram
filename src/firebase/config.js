288ba5ed57010afb9dd9eabf45529984966f894b
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-02-18 19:25:14 +0000

    config.js created

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAaHj4UtVVQqJhYutKQZAIazwnY5qycfj8",
+    authDomain: "sebagram-e215d.firebaseapp.com",
+    projectId: "sebagram-e215d",
+    storageBucket: "sebagram-e215d.appspot.com",
+    messagingSenderId: "520368753558",
+    appId: "1:520368753558:web:5a182ea9bd138a36c26bb7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
