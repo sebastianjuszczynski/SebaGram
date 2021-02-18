52fde36fe74891a0255972dcbc23a6697db6d94b
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-02-18 20:39:58 +0000

    timestamp added to config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
-import * as firebase from "firebase/app";
-import "firebase/storage";
-import "firebase/firestore";
+import firebase from "firebase/app";
+import 'firebase/storage';
+import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -16,5 +16,6 @@ firebase.initializeApp(firebaseConfig);
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectStorage, projectFirestore };
+export { projectStorage, projectFirestore, timestamp };
